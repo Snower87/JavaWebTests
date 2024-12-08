11ffb3044000ddfbd89cc5dd1877a9444e23e6f2
Sergei Begletsov <snower87@yandex.ru>
2024-12-08 10:18:52 +0500

    add 4 test: 2 - with wrong just login or password, 2 - with empty login/password

--- src/test/java/tests/LoginWithWrongCredentials.java
+++ src/test/java/tests/LoginWithWrongCredentials.java
@@ -31,5 +31,66 @@ class LoginWithWrongCredentials extends BaseTest {
         //Проверка текста сообщения об ошибке
         String expectedErrorMessage = "Неправильно указан логин и/или пароль";
         String actualErrorMessage = loginPage.getErrorMessageText();
-        assertEquals(expectedErrorMessage, actualErrorMessage, "Текст сообщения об ошибке не совпадает");    }
+        assertEquals(expectedErrorMessage, actualErrorMessage, "Текст сообщения об ошибке не совпадает");
+    }
+
+    @Test
+    public void loginWithWrongLogin() {
+        //Попытка входа с некорректными данными
+        loginPage.writeLogin("incorrectUser");
+        loginPage.clickLoginButton();
+
+        //Проверка наличия сообщения об ошибке
+        assertTrue(loginPage.isErrorMessageVisible(), "Сообщение об ошибке входа не отображается");
+
+        //Проверка текста сообщения об ошибке
+        String expectedErrorMessage = "Введите пароль";
+        String actualErrorMessage = loginPage.getErrorMessageText();
+        assertEquals(expectedErrorMessage, actualErrorMessage, "Текст сообщения об ошибке не совпадает");
+    }
+
+    @Test
+    public void loginWithWrongPassword() {
+        //Попытка входа с некорректными данными
+        loginPage.writePassword("incorrectPassword");
+        loginPage.clickLoginButton();
+
+        //Проверка наличия сообщения об ошибке
+        assertTrue(loginPage.isErrorMessageVisible(), "Сообщение об ошибке входа не отображается");
+
+        //Проверка текста сообщения об ошибке
+        String expectedErrorMessage = "Введите логин";
+        String actualErrorMessage = loginPage.getErrorMessageText();
+        assertEquals(expectedErrorMessage, actualErrorMessage, "Текст сообщения об ошибке не совпадает");
+    }
+
+    @Test
+    public void loginWithEmptyLogin() {
+        //Попытка входа с некорректными данными
+        loginPage.writeLogin("");
+        loginPage.clickLoginButton();
+
+        //Проверка наличия сообщения об ошибке
+        assertTrue(loginPage.isErrorMessageVisible(), "Сообщение об ошибке входа не отображается");
+
+        //Проверка текста сообщения об ошибке
+        String expectedErrorMessage = "Введите логин";
+        String actualErrorMessage = loginPage.getErrorMessageText();
+        assertEquals(expectedErrorMessage, actualErrorMessage, "Текст сообщения об ошибке не совпадает");
+    }
+
+    @Test
+    public void loginWithEmptyPassword() {
+        //Попытка входа с некорректными данными
+        loginPage.writePassword("");
+        loginPage.clickLoginButton();
+
+        //Проверка наличия сообщения об ошибке
+        assertTrue(loginPage.isErrorMessageVisible(), "Сообщение об ошибке входа не отображается");
+
+        //Проверка текста сообщения об ошибке
+        String expectedErrorMessage = "Введите логин";
+        String actualErrorMessage = loginPage.getErrorMessageText();
+        assertEquals(expectedErrorMessage, actualErrorMessage, "Текст сообщения об ошибке не совпадает");
+    }
 }
